0d26c5b2f73fece19c6e911c9b5c918445b49ea4
Vladislav  Zinovyev <vladozdeveloper@gmail.com>
2023-04-05 23:40:54 +0300

    updated test a function that squares a number

--- 1_jest/src/square/square.js
+++ 1_jest/src/square/square.js
@@ -1,7 +1,13 @@
 //функция, которая возводит число в квадрат
-const square = (number) => {
+/* const square = (number) => {
     return number*number
+} */
+
+const square = (number) => {
+    if(number === 1) {
+        return 1;
+    }
+    return Math.pow(number, 2);
 }
 
 module.exports = square;
-
--- 1_jest/src/square/square.test.js
+++ 1_jest/src/square/square.test.js
@@ -1,6 +1,6 @@
 const square = require('./square');
 
-describe('square', () => {
+/* describe('square', () => {
     test('Корректное значение', () => {
         expect(square(2)).toBe(4); //число, которое должны получить
         expect(square(2)).toBeLessThan(5); //число, которое получили меньше, чем 5
@@ -8,6 +8,41 @@ describe('square', () => {
         expect(square(2)).not.toBeUndefined(); //число, которое получили не indefined
         
     })
+}) */
+
+describe('square', () => {
+    let mockValue;
+    // Перед каждым запуском теста
+    beforeEach(() => {
+        // ДОБАВИТЬ В БД
+    })
+    // Один раз перед всеми тестами
+    beforeAll(() => {
+    })
+    test('Корректное значение', () => {
+        // expect(square(2)).toBe(4);
+        // expect(square(2)).toBeLessThan(5);
+        // expect(square(2)).toBeGreaterThan(3);
+        // expect(square(2)).not.toBeUndefined();
+        const spyMathPow = jest.spyOn(Math, 'pow');
+        square(2);
+        expect(spyMathPow).toBeCalledTimes(1);
+    })
+
+    test('Корректное значение', () => {
+        const spyMathPow = jest.spyOn(Math, 'pow');
+        square(1);
+        expect(spyMathPow).toBeCalledTimes(0);
+    })
+
+    //выполняется после каждого теста
+    afterEach(() => {
+        //очищаем тесты
+        jest.clearAllMocks()
+    })
+
+    //выполняется после всех тестов
+    afterAll(() => {
+
+    })
 })
-
-
