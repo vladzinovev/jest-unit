fbfb74df8c58c958c790e28946d2450894ae8df9
Vladislav  Zinovyev <vladozdeveloper@gmail.com>
2023-04-06 23:58:32 +0300

    deployed a react application for testing

--- 1_jest/src/getData/getData.js
+++ 1_jest/src/getData/getData.js
@@ -6,7 +6,8 @@ const getData = async () => {
     try {
         const response = await axios.get('https://jsonplaceholder.typicode.com/users');
         const userIds = response.data.map(user => user.id)
-        return mapArrToStrings(userIds);
+        //return mapArrToStrings(userIds);
+        return userIds;
     } catch (e) {
 
     }
