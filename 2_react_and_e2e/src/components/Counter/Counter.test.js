ea9f163aecddc3c71d171321281f5ca1062c42a9
Vladislav  Zinovyev <vladozdeveloper@gmail.com>
2023-04-12 23:05:13 +0300

    added counter test

---
+++ 2_react_and_e2e/src/components/Counter/Counter.test.js
@@ -0,0 +1,19 @@
+import {render, screen} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import {renderTestApp} from "../../tests/helpers/renderTestApp";
+
+describe('Counter test', () => {
+    test('Test router', async () => {
+        const {getByTestId} = render(renderTestApp(null, {
+            route: '/',
+            initialState: {
+                counter: {value: 10}
+            }
+        }))
+
+        const incrementBtn = getByTestId('increment-btn')
+        expect(getByTestId('value-title')).toHaveTextContent('10')
+        userEvent.click(incrementBtn);
+        expect(getByTestId('value-title')).toHaveTextContent('11')
+    });
+})
