215531c28f518320e11f2ccad9411f7ef20b041e
Vladislav  Zinovyev <vladozdeveloper@gmail.com>
2023-04-19 19:43:38 +0300

    added test

--- 2_react_and_e2e/src/components/users/Users.test.js
+++ 2_react_and_e2e/src/components/users/Users.test.js
@@ -2,6 +2,7 @@ import { render, screen} from '@testing-library/react';
 import Users from "./Users.js";
 import axios from 'axios';
 import React from 'react';
+import userEvent from '@testing-library/user-event';
 
 jest.mock('axios');
 
@@ -26,6 +27,10 @@ describe('USERS TEST', () => {
         }
     })
 
+    afterEach(() => {
+        jest.clearAllMocks(); //отключаем mock после каждого теста
+    })
+
     test('renders learn react link', async() => {
         //якобы получаем через axios запрос данные
         // @ts-ignore
@@ -39,5 +44,18 @@ describe('USERS TEST', () => {
         // eslint-disable-next-line testing-library/no-debugging-utils
         screen.debug();
     });
+
+    test('test redirect to details page', async() => {
+        // @ts-ignore
+        axios.get.mockReturnValue(response);
+        // @ts-ignore
+        // eslint-disable-next-line no-undef
+        render(renderWithRouter(<Users />));
+        const users = await screen.findAllByTestId('user-item');
+        expect(users.length).toBe(3);
+        userEvent.click(users[0])//кликаем на любого пользователя
+        expect(screen.getByTestId('user-page')).toBeInTheDocument();//ожидаем что открылась стараница
+    });
 })
+
 
