e43b5c912239d9eb3f330ed6dcb1a9d7dbc95416
Vladislav  Zinovyev <vladozdeveloper@gmail.com>
2023-04-25 23:58:48 +0300

    rewrited app

--- 2_react_and_e2e/src/App.test.tsx
+++ 2_react_and_e2e/src/App.test.tsx
@@ -1,9 +1,10 @@
+import { render, screen,fireEvent } from '@testing-library/react';
+import  userEvent  from '@testing-library/user-event';
 import React from 'react';
-import { render, screen } from '@testing-library/react';
 import App from './App';
 
 describe('TEST APP', () => {
-  test('renders learn react link', () => {
+  /* test('renders learn react link', () => {
     //компонент который будем тестировать
     render(<App />);
     //получаем элемент по тексту hello world (i-игнорируем регистр)
@@ -20,6 +21,47 @@ describe('TEST APP', () => {
     
     // eslint-disable-next-line testing-library/no-debugging-utils
     screen.debug();//возвращает разметку
-  }); 
+  }); */
   
+  test('for component data', async () => {
+    //компонент который будем тестировать
+    render(<App />);
+
+    // eslint-disable-next-line testing-library/no-debugging-utils
+    screen.debug();//возвращает разметку    
+
+    //получаем элемент по тексту data (i-игнорируем регистр)
+    const data =await screen.findByText(/data/i);
+
+    //toBeInTheDocument появиля ли в нашем документе
+    expect(data).toBeInTheDocument();
+    expect(data).toHaveStyle({color: 'red'});
+    // eslint-disable-next-line testing-library/no-debugging-utils
+    screen.debug();//возвращает разметку
+  });
+
+  test("CLICK EVENT", () => {
+    render(<App />);
+    const btn = screen.getByTestId('toggle-btn');
+    expect(screen.queryByTestId('toggle-elem')).toBeNull();//tobenull потому что элемента еще нет
+    fireEvent.click(btn);//для работы с событиями
+    // eslint-disable-next-line testing-library/prefer-presence-queries
+    expect(screen.queryByTestId('toggle-elem')).toBeInTheDocument();//ожидаем что он уже появился на странице
+    fireEvent.click(btn);//для работы с событиями
+    expect(screen.queryByTestId('toggle-elem')).toBeNull();
+  })
+  
+  test("INPUT EVENT", () => {
+    render(<App />);
+    const input = screen.getByPlaceholderText(/input value/i)
+    expect(screen.queryByTestId('value-elem')).toContainHTML('');
+    // Искуственное событие
+    /* fireEvent.input(input, {
+       target: {value: '123123'}
+    }) */
+    // Близко к пользователю, обрабатываются события нажатия клавиш и тд
+    userEvent.type(input, '123123')
+    
+    expect(screen.queryByTestId('value-elem')).toContainHTML('123123');
+  })
 });
--- 2_react_and_e2e/src/App.tsx
+++ 2_react_and_e2e/src/App.tsx
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-function App() {
+const App = () => {
+  const [data, setData] = useState(null);
+  const [toggle, setToggle] = useState(false);
+  const [value, setValue] = useState("");
+
+  const onClick = () => setToggle((prev) => !prev);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setData({});
+    }, 100);
+  }, []);
+
   return (
     <div>
+      <h1 data-testid="value-elem">{value}</h1>
+      {toggle === true && <div data-testid="toggle-elem">toggle</div>}
+      {data && <div style={{ color: "red" }}>data</div>}
       <h1>Hello world</h1>
-      <button data-testid="toggle-btn">
+      <button data-testid="toggle-btn" onClick={onClick}>
         click me
       </button>
       <input
+        onChange={(e) => setValue(e.target.value)}
         type="text"
         placeholder="input value....."
       />
     </div>
   );
-}
+};
 
 export default App;
