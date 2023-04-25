9e7aa3fa69180393ea61ea7ed47296c58a44c535
Vladislav  Zinovyev <vladozdeveloper@gmail.com>
2023-04-25 23:49:51 +0300

    recreated in typescript

---
+++ 2_react_and_e2e/src/App.test.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+describe('TEST APP', () => {
+  test('renders learn react link', () => {
+    //компонент который будем тестировать
+    render(<App />);
+    //получаем элемент по тексту hello world (i-игнорируем регистр)
+    const helloWorldElem = screen.getByText(/hello world/i);
+    //элемент кнопка
+    const btn = screen.getByRole('button')
+    //элемент input
+    const input = screen.getByPlaceholderText(/input value/i)
+
+    //toBeInTheDocument появиля ли в нашем документе
+    expect(helloWorldElem).toBeInTheDocument();
+    expect(btn).toBeInTheDocument();
+    expect(input).toMatchSnapshot();
+    
+    // eslint-disable-next-line testing-library/no-debugging-utils
+    screen.debug();//возвращает разметку
+  }); 
+  
+});
---
+++ 2_react_and_e2e/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <button data-testid="toggle-btn">
+        click me
+      </button>
+      <input
+        type="text"
+        placeholder="input value....."
+      />
+    </div>
+  );
+}
+
+export default App;
