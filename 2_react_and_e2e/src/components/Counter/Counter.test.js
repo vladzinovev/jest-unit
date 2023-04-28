be8128c264c9d282a3b30ba2dac95c8434fd20fb
Vladislav  Zinovyev <vladozdeveloper@gmail.com>
2023-04-28 23:57:29 +0300

    added render with redux

--- 2_react_and_e2e/src/components/Counter/Counter.test.js
+++ 2_react_and_e2e/src/components/Counter/Counter.test.js
@@ -3,21 +3,18 @@ import Counter from "./Counter";
 import userEvent from "@testing-library/user-event";
 import {Provider} from "react-redux";
 import {createReduxStore} from "../../store/store";
-import {renderWithRedux} from "../../tests/helpers/renderWithRedux";
-import {renderTestApp} from "../../tests/helpers/renderTestApp";
+import { renderWithRedux } from "../../tests/helpers/renderWithRedux";
 
 describe('Counter test', () => {
     test('Test router', async () => {
-        const {getByTestId} = render(renderTestApp(null, {
-            route: '/',
-            initialState: {
-                counter: {value: 10}
-            }
-        }))
+        const {getByTestId} = render(renderWithRedux(<Counter/>,{counter:{value:10}}))
 
+        // eslint-disable-next-line testing-library/prefer-screen-queries
         const incrementBtn = getByTestId('increment-btn')
+        // eslint-disable-next-line testing-library/prefer-screen-queries
         expect(getByTestId('value-title')).toHaveTextContent('10')
         userEvent.click(incrementBtn);
+        // eslint-disable-next-line testing-library/prefer-screen-queries
         expect(getByTestId('value-title')).toHaveTextContent('11')
     });
 })
