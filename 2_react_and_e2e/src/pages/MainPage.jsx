e6b16869d7d979d66ec9631d9e31446485e781ba
Vladislav  Zinovyev <vladozdeveloper@gmail.com>
2023-04-28 23:37:45 +0300

    added counterReducer test

--- 2_react_and_e2e/src/pages/MainPage.jsx
+++ 2_react_and_e2e/src/pages/MainPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Counter from '../components/Counter/Counter';
 
 const MainPage = () => {
     return (
         <div data-testid="main-page">
             MAIN PAGE
+            <Counter />
         </div>
     );
 };
