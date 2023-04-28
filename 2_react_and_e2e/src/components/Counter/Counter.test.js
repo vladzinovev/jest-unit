e351370aa9d8bc1a318e70014967f2754caf66ad
Vladislav  Zinovyev <vladozdeveloper@gmail.com>
2023-04-29 00:08:48 +0300

    installed wdio

--- 2_react_and_e2e/src/components/Counter/Counter.test.js
+++ 2_react_and_e2e/src/components/Counter/Counter.test.js
@@ -1,9 +1,7 @@
 import {render, screen} from "@testing-library/react";
-import Counter from "./Counter";
 import userEvent from "@testing-library/user-event";
-import {Provider} from "react-redux";
-import {createReduxStore} from "../../store/store";
 import { renderWithRedux } from "../../tests/helpers/renderWithRedux";
+import { renderTestApp } from "../../tests/helpers/renderTestApp";
 
 describe('Counter test', () => {
     test('Test router', async () => {
